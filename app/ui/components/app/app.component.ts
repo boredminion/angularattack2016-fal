5b070bc4edc63b079c205623007c728a971f1c59
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-15 00:23:25 +0530

    Added list challenge component as dependency

--- app/ui/components/app/app.component.ts
+++ app/ui/components/app/app.component.ts
@@ -3,6 +3,7 @@ import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {Navigation} from '../navigation/navigation.component';
 import {HomeTitleComponent} from '../home-title/home-title.component';
 import { CodeEditorComponent } from '../code-editor/code-editor.component';
+import { ListChallengesComponent } from '../list-challenges/list-challenges.component';
 
 @Component({
     selector: 'my-app',
@@ -11,13 +12,15 @@ import { CodeEditorComponent } from '../code-editor/code-editor.component';
       <md-content>
         <home-title></home-title>
         <code-editor></code-editor>
+        <list-challenges></list-challenges>
       </md-content>
     `,
     directives: [
       MATERIAL_DIRECTIVES,
       Navigation,
       HomeTitleComponent,
-      CodeEditorComponent
+      CodeEditorComponent,
+      ListChallengesComponent
     ]
 })
 
