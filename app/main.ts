971ceec081bfc29de83e374ac8c1c0dbd7bfe3bd
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-14 20:20:40 +0530

    Added MATERIAL_PROVIDERS intialize in main

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,8 @@
 import {bootstrap}  from 'angular2/platform/browser';
-import {AppComponent} from './app/app.component';
+import {AppComponent} from './ui/components/app/app.component';
+import {MATERIAL_PROVIDERS} from 'ng2-material/all';
 import {enableProdMode} from 'angular2/core';
 
 enableProdMode();
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [MATERIAL_PROVIDERS]);
