4f60b1e9b2297f1daaaa34977521865af27e4a1e
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-14 20:53:54 +0530

    Added home title component

--- app/ui/components/app/app.component.ts
+++ app/ui/components/app/app.component.ts
@@ -1,19 +1,17 @@
 import {Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
 import {Navigation} from '../navigation/navigation.component';
+import {HomeTitleComponent} from '../home-title/home-title.component';
 
 @Component({
     selector: 'my-app',
     template: `
       <navigation></navigation>
       <md-content>
-        <h1>
-          AngularAttack 2016
-        </h1>
-        <p>Your project runs!!!</p>
+        <home-title></home-title>
       </md-content>
     `,
-    directives: [MATERIAL_DIRECTIVES, Navigation]
+    directives: [MATERIAL_DIRECTIVES, Navigation, HomeTitleComponent]
 })
 
 export class AppComponent { }
---
+++ app/ui/components/home-title/home-title.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+
+@Component({
+  selector: 'home-title',
+  template: `
+    <h2>Welcome to Code Wars</h2>
+  `
+})
+
+export class HomeTitleComponent { }
