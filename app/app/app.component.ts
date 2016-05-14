2fe08520101b31a6d0a93cdd260d48fc92ad594a
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-14 13:49:05 +0530

    Updated to typescript & added compiled code

---
+++ app/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+      <h1>AngularAttack 2016
+      </h1><p>Your project runs!!!</p>
+    `
+})
+
+export class AppComponent { }
--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
-(function(app) {
-  document.addEventListener('DOMContentLoaded', function() {
-    ng.platformBrowserDynamic.bootstrap(app.AppComponent);
-  });
-})(window.app || (window.app = {}));
+"use strict";
+var browser_1 = require('angular2/platform/browser');
+var app_component_1 = require('./app/app.component');
+var core_1 = require('angular2/core');
+core_1.enableProdMode();
+browser_1.bootstrap(app_component_1.AppComponent);
+//# sourceMappingURL=main.js.map
