f7c96c1d378ea9c8b46a469167b3ac49adec536b
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-14 20:22:10 +0530

    Added navigation component ts file

---
+++ app/ui/components/navigation/navigation.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "angular2/core";
+import {MATERIAL_DIRECTIVES} from "ng2-material/all";
+
+@Component({
+  selector: 'navigation',
+  templateUrl: 'app/ui/components/navigation/navigation.html',
+  directives: [MATERIAL_DIRECTIVES]
+})
+
+export class Navigation { }
