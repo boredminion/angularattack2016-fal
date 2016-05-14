a18eeb49add94c4cbc23e524bb05d2832900adeb
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-14 20:19:48 +0530

    Deleted the compiled files

--- app/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var browser_1 = require('angular2/platform/browser');
-var app_component_1 = require('./app/app.component');
-var core_1 = require('angular2/core');
-core_1.enableProdMode();
-browser_1.bootstrap(app_component_1.AppComponent);
-//# sourceMappingURL=main.js.map
