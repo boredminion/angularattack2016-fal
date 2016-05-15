bf87b32f6122761c1f5c5e01a0260d2a192d5d23
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 02:06:21 +0530

    Added code-editor component

--- app/ui/components/show-challenge/show-challenge.component.ts
+++ app/ui/components/show-challenge/show-challenge.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from "angular2/core";
-import { Challenge } from "../../service/challenge/challenge";
 import { RouteParams } from 'angular2/router';
+
+import { Challenge } from "../../service/challenge/challenge";
 import { ChallengeService } from '../../service/challenge/challenge.service';
+
+import { CodeEditorComponent } from '../code-editor/code-editor.component';
 
 @Component({
   selector: 'show-challenge',
   template: `
+    <code-editor></code-editor>
     <div *ngIf="challenge">
       <h3>{{challenge.name}}</h3>
       <p>{{challenge.description}}</p>
     </div>
     <button (click)="goBack()">Back</button>
-  `
+  `,
+  directives : [
+    CodeEditorComponent
+  ]
 })
 
 export class ShowChallengeComponent implements OnInit {
