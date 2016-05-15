3a46e9f7fb5ef1c1513d524c910989a4dd612703
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 03:48:15 +0530

    Updated the view with material design

--- app/ui/components/list-challenges/list-challenges.component.ts
+++ app/ui/components/list-challenges/list-challenges.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from 'angular2/core';
 import { Router } from 'angular2/router';
+import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 
 import { Challenge } from '../../service/challenge/challenge';
 import { ChallengeService } from '../../service/challenge/challenge.service';
@@ -7,12 +8,25 @@ import { ChallengeService } from '../../service/challenge/challenge.service';
 @Component({
   selector: 'list-challenges',
   template: `
-    <ul>
-      <li *ngFor="let challenge of challenges | async" (click)="onSelect(challenge)">
-        {{challenge.name}}
-      </li>
-    </ul>
-  `
+  <md-card *ngFor="let challenge of challenges | async">
+    <md-card-title>
+      <md-card-title-text>
+        <span class="md-headline">{{challenge.name}}</span>
+      </md-card-title-text>
+    </md-card-title>
+    <md-card-actions layout="row" layout-align="end">
+      <button md-button (click)="onSelect(challenge)">Start</button>
+    </md-card-actions>
+    <md-card-content>
+      <p>
+        {{challenge.description}}
+      </p>
+    </md-card-content>
+  </md-card>
+  `,
+  directives: [
+    MATERIAL_DIRECTIVES
+  ]
 })
 
 export class ListChallengesComponent implements OnInit {
