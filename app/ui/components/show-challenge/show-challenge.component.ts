c69d7acec646630a790facdc896ba57a7ff83d2d
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 02:02:00 +0530

    Added challenge fetch service

--- app/ui/components/show-challenge/show-challenge.component.ts
+++ app/ui/components/show-challenge/show-challenge.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from "angular2/core";
+import { Component, OnInit } from "angular2/core";
 import { Challenge } from "../../service/challenge/challenge";
+import { RouteParams } from 'angular2/router';
+import { ChallengeService } from '../../service/challenge/challenge.service';
 
 @Component({
   selector: 'show-challenge',
@@ -8,9 +10,21 @@ import { Challenge } from "../../service/challenge/challenge";
       <h3>{{challenge.name}}</h3>
       <p>{{challenge.description}}</p>
     </div>
+    <button (click)="goBack()">Back</button>
   `
 })
 
-export class ShowChallengeComponent {
-  @Input() challenge = Challenge;
+export class ShowChallengeComponent implements OnInit {
+  challenge: Challenge;
+
+  constructor(private routeParams: RouteParams, private challengeService: ChallengeService) {}
+
+  ngOnInit(){
+    let id = +this.routeParams.get('id');
+    this.challengeService.getChallenge(id).then(challenge => this.challenge = challenge);
+  }
+
+  goBack() {
+     window.history.back();
+   }
 }
