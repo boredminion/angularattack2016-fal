5803c693cb02b7e76be041ccd851de53a0054659
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-15 13:23:23 +0530

    Added angular fire to challenge service

--- app/ui/service/challenge/challenge.service.ts
+++ app/ui/service/challenge/challenge.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from 'angular2/core';
 import { Challenge} from './challenge';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 import { CHALLENGES } from './mock-challenges';
 
 @Injectable()
 export class ChallengeService {
+  challenges: FirebaseListObservable<any>;
+
+  constructor(private af: AngularFire) {
+    this.challenges = af.database.list('/challenges');
+  }
+
   getChallenges() {
       return Promise.resolve(CHALLENGES);
+  }
+
+  listChallenges() {
+    return Promise.resolve(this.challenges);
+  }
+
+  addChallenge(challenge: Challenge) {
+    this.challenges.push(challenge);
   }
 }
