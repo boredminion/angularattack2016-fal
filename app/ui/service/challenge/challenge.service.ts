a2ea4619b42eba710d64d0f4163b0d8152f19196
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 05:01:36 +0530

    Added observable for filter of firebase data

--- app/ui/service/challenge/challenge.service.ts
+++ app/ui/service/challenge/challenge.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from 'angular2/core';
 import { Challenge} from './challenge';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Subject } from 'rxjs/Subject';
 
 import { CHALLENGES } from './mock-challenges';
 
 @Injectable()
 export class ChallengeService {
   challenges: FirebaseListObservable<any>;
+  challenge: FirebaseListObservable<any>;
+  id: Subject<any>;
 
   constructor(private af: AngularFire) {
+    this.id = new Subject();
+
     this.challenges = af.database.list('/challenges');
+    this.challenge = af.database.list('/challenges', {
+      query: {
+        orderByChild: 'id',
+        equalTo: this.id
+      }
+    });
   }
 
   getChallenges() {
@@ -22,5 +33,10 @@ export class ChallengeService {
 
   addChallenge(challenge: Challenge) {
     this.challenges.push(challenge);
+  }
+
+  getChallenge(id){
+    this.id.next(id);
+    return Promise.resolve(this.challenge);
   }
 }
