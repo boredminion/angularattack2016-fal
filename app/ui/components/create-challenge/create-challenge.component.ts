ba637b8c1813676819fdfea06d3a1607a250c076
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-15 13:22:22 +0530

    Added new module to create challenge

---
+++ app/ui/components/create-challenge/create-challenge.component.ts
@@ -0,0 +1,24 @@
+import { Component } from 'angular2/core';
+import { Challenge } from '../../service/challenge/challenge';
+import { ChallengeService } from '../../service/challenge/challenge.service';
+
+@Component({
+  selector: 'create-challenge',
+  template: `
+  <h2>Create new challenge</h2>
+  <input type="text" #newitem />
+  <textarea type="text" #newdescription></textarea>
+  <button (click)="add(newitem.value, newdescription.value)">Add</button>
+  `,
+  providers : [ ChallengeService ]
+})
+
+export class CreateChallengeComponent {
+  challenge: Challenge;
+
+  constructor(private challengeService: ChallengeService) { }
+
+  add(newName: string, newDescription: string) {
+    this.challengeService.addChallenge({ name: newName, description: newDescription });
+  }
+}
