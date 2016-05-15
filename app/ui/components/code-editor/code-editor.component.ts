d5720042a426b434c8c11382619eea8e96462f79
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 00:19:43 +0530

    Added firebase sync data with observables

--- app/ui/components/code-editor/code-editor.component.ts
+++ app/ui/components/code-editor/code-editor.component.ts
@@ -1,18 +1,51 @@
-import { Component, Input } from "angular2/core";
+import { Component, OnInit, Input } from "angular2/core";
+import { Control, FormBuilder, Validators, ControlGroup } from 'angular2/common';
 import { Code } from "../../service/code-service/code";
+import { CodeService } from '../../service/code-service/code.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'code-editor',
   template: `
-    <textarea type="textarea" [(ngModel)]="codeData.code" flex></textarea>
-    <p>{{codeData.code}}</p>
-  `
+  <p>{{ message$ | async }}</p>
+  <textarea type="text" [ngFormControl]="code"></textarea>
+  `,
+  providers: [ CodeService ]
 })
 
-export class CodeEditorComponent {
-  codeData : Code = {
-    id: 1,
-    code: 'test'
-  };
+export class CodeEditorComponent implements OnInit {
+  code = new Control("");
+  codeData : Observable<any>;
+  message$:Observable<string>;
+
+  resend() {
+    this.message$ = Observable.interval(2000)
+      .map(i => this.messages[i])
+      .take(this.messages.length + 1);
+  }
+
+  private messages = [
+    'Welcome Code Warrior',
+    'Here you will be solving a time bound challenge with your pair partner',
+    'Begin once the clock starts ticking'
+  ];
+
+  getCodeData () {
+    this.codeData = this.codeService.getCode();
+    this.codeData.subscribe(data => this.code.updateValue(data.code));
+  }
+
+  add(code: string){
+    this.codeService.saveCode({ id: 1, code: code});
+  }
+
+  ngOnInit(){
+    this.getCodeData();
+  }
+
+  constructor(private codeService: CodeService) {
+    this.code.valueChanges.debounceTime(400).subscribe(term => this.codeService.saveCode({ id: 1, code: term}));
+    this.resend();
+  }
   // @Input() codeData: Code;
 }
