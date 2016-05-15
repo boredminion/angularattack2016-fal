f3de294e0fcb7b298ccc0036fe0b1b271c2fc304
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 01:14:22 +0530

    Updated Navigation component to have home url

--- app/ui/components/navigation/navigation.component.ts
+++ app/ui/components/navigation/navigation.component.ts
@@ -1,10 +1,14 @@
 import {Component} from "angular2/core";
+import { ROUTER_DIRECTIVES } from 'angular2/router';
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
 
 @Component({
   selector: 'navigation',
   templateUrl: 'app/ui/components/navigation/navigation.html',
-  directives: [MATERIAL_DIRECTIVES]
+  directives: [
+    MATERIAL_DIRECTIVES,
+    ROUTER_DIRECTIVES
+  ]
 })
 
 export class Navigation { }
