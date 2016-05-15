5d980489875efca24b20efac2770895c99c96155
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 00:20:18 +0530

    Updated the service to use firebase datastore

--- app/ui/service/code-service/code.service.ts
+++ app/ui/service/code-service/code.service.ts
@@ -1,4 +1,29 @@
+import { Injectable } from 'angular2/core';
+import { Code } from './code';
+import { AngularFire, FirebaseObjectObservable } from 'angularfire2';
+
+
+@Injectable()
 export class CodeService {
-  id: number;
-  code: string
+  code: FirebaseObjectObservable<any>;
+
+  constructor(private af: AngularFire) {
+    this.code = af.database.object('/myCode');
+  }
+
+  getCode() {
+      return this.code;
+  }
+
+  saveCode(code: Code) {
+    this.code.set(code);
+  }
+
+  updateCode(code: Code) {
+    this.code.update(code);
+  }
+
+  deleteCode() {
+    this.code.remove();
+  }
 }
