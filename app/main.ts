6a47233979b8c2e8b789bc31516546c2f3ea7b0a
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-15 13:24:06 +0530

    Added AngularFire settings to main project

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,14 @@ import {bootstrap}  from 'angular2/platform/browser';
 import {AppComponent} from './ui/components/app/app.component';
 import {MATERIAL_PROVIDERS} from 'ng2-material/all';
 import {enableProdMode} from 'angular2/core';
+// import {ROUTER_PROVIDERS} from 'angular2/router';
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
 
 enableProdMode();
 
-bootstrap(AppComponent, [MATERIAL_PROVIDERS]);
+bootstrap(AppComponent, [
+  MATERIAL_PROVIDERS,
+  FIREBASE_PROVIDERS,
+  defaultFirebase('https://crackling-heat-9094.firebaseio.com/')
+  // ROUTER_PROVIDERS
+]);
