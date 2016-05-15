6bbea9fbf31bed7a4530bea1012a09a9978c852a
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 04:46:28 +0530

    Updated navigation & routes

--- app/ui/components/app/app.component.ts
+++ app/ui/components/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
 import {Navigation} from '../navigation/navigation.component';
+
+import { ChallengeService } from '../../service/challenge/challenge.service';
+import { CodeService } from '../../service/code-service/code.service';
+
 import { CodeChallenge } from '../code-challenge/code-challenge.component';
+import { ShowChallengeComponent } from '../show-challenge/show-challenge.component';
+import { CreateChallengeComponent } from '../create-challenge/create-challenge.component';
 
 @Component({
   selector: 'my-app',
@@ -11,11 +18,15 @@ import { CodeChallenge } from '../code-challenge/code-challenge.component';
   `,
   directives: [
     ROUTER_DIRECTIVES,
+    Navigation,
     CodeChallenge,
-    Navigation
+    ShowChallengeComponent,
+    CreateChallengeComponent
   ],
   providers : [
-    ROUTER_PROVIDERS
+    ROUTER_PROVIDERS,
+    ChallengeService,
+    CodeService
   ]
 })
 
@@ -25,6 +36,16 @@ import { CodeChallenge } from '../code-challenge/code-challenge.component';
     name: 'Challenges',
     component: CodeChallenge,
     useAsDefault: true
+  },
+  {
+    path: '/challenges/:id',
+    name: 'Challenge',
+    component: ShowChallengeComponent
+  },
+  {
+    path: '/challenge/add',
+    name: 'ChallengeAdd',
+    component: CreateChallengeComponent
   }
 ])
 
