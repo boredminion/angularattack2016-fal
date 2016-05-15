2de2051f26b940b788c5d47c1713d054a49b7e8a
Tyson Thomas <tyson.thomas@caspen.co.in>
2016-05-16 02:06:50 +0530

    Moved the code editor component

--- app/ui/components/code-challenge/code-challenge.component.ts
+++ app/ui/components/code-challenge/code-challenge.component.ts
@@ -1,28 +1,24 @@
 import {Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from 'ng2-material/all';
-import {Navigation} from '../navigation/navigation.component';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+
 import {HomeTitleComponent} from '../home-title/home-title.component';
-import { CodeEditorComponent } from '../code-editor/code-editor.component';
 import { ListChallengesComponent } from '../list-challenges/list-challenges.component';
-import { CreateChallengeComponent } from '../create-challenge/create-challenge.component';
 
 @Component({
     selector: 'code-challenge',
     template: `
       <md-content>
         <home-title></home-title>
-        <code-editor></code-editor>
         <list-challenges></list-challenges>
-        <create-challenge></create-challenge>
+        <a type="button" [routerLink]="['ChallengeAdd']" style="padding-right:5px;">Creat Challenge</a>
       </md-content>
     `,
     directives: [
       MATERIAL_DIRECTIVES,
-      Navigation,
+      ROUTER_DIRECTIVES,
       HomeTitleComponent,
-      CodeEditorComponent,
-      ListChallengesComponent,
-      CreateChallengeComponent
+      ListChallengesComponent
     ]
 })
 
